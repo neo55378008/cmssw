c35576bb1e34bb1b764a88dbc77292b7a3dba1d7
Brent <brent.yates@email.ucr.edu>
2015-04-09 19:15:57 +0200

    modified:   L1TDisk.hh

--- SimDataFormats/SLHC/interface/L1TDisk.hh
+++ SimDataFormats/SLHC/interface/L1TDisk.hh
@@ -342,6 +342,8 @@ public:
 	      if (fabs(deltaz)>zcut2) continue;
 	      dist=hypot(rdeltaphi/(rphicut2*phiSF),deltaz/zcut2);
             }
+	    else
+              dist=hypot(rdeltaphi/(0.1*phiSF),deltaz/5.0); //LS modified from 0.5 to 5.0*/
 
 	    if (dist<bestdist) {
 	      bestdist=dist;
